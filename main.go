8af1933cf0eb23bdcf66ac83589a16690e215cb4
juzhanglei 00234709 <jack.ju@itiky.com>
2019-09-08 17:27:39 +0800

    Support generate file generate.go

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
+	"syscall"
 )
 
 func main() {
@@ -18,8 +22,29 @@ func main() {
 		"runtime/var/log",
 	}
 
+	mask := syscall.Umask(0)
+	defer syscall.Umask(mask)
+
 	for _, dir := range dirs {
 		fmt.Println(dir)
-		os.MkdirAll(dir, os.ModeDir)
+		os.MkdirAll(dir, 0755)
 	}
+
+	var content []string
+	content = append(content, "package main\n")
+	filepath.Walk("api", func(path string, f os.FileInfo, err error) error {
+		if f == nil {
+			return nil
+		}
+		if f.IsDir() {
+			return nil
+		}
+
+		if strings.HasSuffix(path, ".raml") {
+			content = append(content, fmt.Sprintf("//go:generate go-raml server --kind gorestful --ramlfile ../%s --no-apidocs", path))
+		}
+		return nil
+	})
+
+	ioutil.WriteFile("code/generate.go", []byte(strings.Join(content, "\n")), 0660)
 }
