0c6acaff72cfff460a23c963a01dcef40702b892
Jack.Ju <jack.ju@itiky.com>
2019-10-17 12:31:58 +0800

    Create version.go

---
+++ commands/version.go
@@ -0,0 +1,12 @@
+package commands
+
+var (
+	//Version define software version
+	Version string
+	// CommitHash represents the Git commit hash at built time
+	CommitHash string
+	// BuildDate represents the date when this tool was built
+	BuildDate string
+	// GoVersion represents the version of build go
+	GoVersion string
+)
